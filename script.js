de6c8afdb14e55812f26b6be2055f0de848183a3
Meow <tabbathapischke@gmail.com>
2023-08-21 12:32:17 -1000

    add const and alerts

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const specialChar = "!#$%&'()*+,-./:;<=>?@[\ ]^_`{|}~\"";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+
 function generatePassword() {
   let passwordChars = "";
   // Prompts- each add specified characters when called upon
   let passLengthPrompt = prompt("Enter password length between 8-128 characters");
   if (passLengthPrompt < 8 || passLengthPrompt > 128) {
-
+    alert("ERROR - Length must be between 8 and 128 characters");
+    return null;
   }
   let lowerCasePrompt = prompt("Include lower case letters? Respond y for yes and n for no.");
   if (lowerCasePrompt === 'y') {
@@ -23,6 +29,10 @@ function generatePassword() {
   let specialCharPrompt = prompt("Include special characters? Respond y for yes and n for no.");
   if (specialCharPrompt === 'y') {
     passwordChars += specialChar;
+  }
+  if (lowerCasePrompt === 'n' && upperCasePrompt === 'n' && numberPrompt === 'n' && specialCharPrompt === 'n') {
+    alert('ERROR - Must include at least one character type');
+    return null;
   }
 
 let generatedPassword = "";
